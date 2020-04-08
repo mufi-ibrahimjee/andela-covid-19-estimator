bc8696179d14d769bfee150e2e788bb6e6955325
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-08 23:24:24 +0530

    indentation sorted

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,15 @@
 const covid19ImpactEstimator = (data) => {
-    //console.log(data)
     const given = data;
     return {
         data: given,
         impact: {
             currentlyInfected: given.reportedCases * 10,
             infectionsByRequestedTime: currentlyInfected * Math.pow(2, given.timeToElapse / 3)
-
         },
         severeImpact: {
             currentlyInfected: given.reportedCases * 50,
             infectionsByRequestedTime: currentlyInfected * Math.pow(2, given.timeToElapse / 3)
         }
-
     };
 };
-
 export default covid19ImpactEstimator;
-//module.exports = covid19ImpactEstimator;
