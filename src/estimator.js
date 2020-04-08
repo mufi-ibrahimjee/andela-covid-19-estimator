2ef81256f4caeedbbda6fcafa41478e0277fa1f3
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-09 01:26:00 +0530

    =time added

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,24 @@
 const covid19ImpactEstimator = (data) => {
   const given = data;
+  var time = 0;
+  if(given.periodType == "days"){
+    time = given.timeToElapse / 3;
+  }
+  if(given.periodType == "weeks"){
+    time = (given.timeToElapse * 7) / 3;
+  }
+  if(given.periodType == "months"){
+    time = (given.timeToElapse * 30) / 3;
+  }
   return {
     data: given,
     impact: {
       currentlyInfected: given.reportedCases * 10,
-      infectionsByRequestedTime: (given.reportedCases * 10) * (2 ** (given.timeToElapse / 3))
+      infectionsByRequestedTime: (given.reportedCases * 10) * (2 ** time)
     },
     severeImpact: {
       currentlyInfected: given.reportedCases * 50,
-      infectionsByRequestedTime: (given.reportedCases * 50) * (2 ** (given.timeToElapse / 3))
+      infectionsByRequestedTime: (given.reportedCases * 50) * (2 ** time)
     }
   };
 };
