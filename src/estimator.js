969eddd62048d3dcc27e26806ee6a7c93126b0ed
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-08 23:13:16 +0530

    challenge1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    //console.log(data)
+    const given = data;
+    return {
+        data: given,
+        impact: {
+            currentlyInfected: given.reportedCases * 10,
+            infectionsByRequestedTime: currentlyInfected * Math.pow(2, given.timeToElapse / 3)
+
+        },
+        severeImpact: {
+            currentlyInfected: given.reportedCases * 50,
+            infectionsByRequestedTime: currentlyInfected * Math.pow(2, given.timeToElapse / 3)
+        }
+
+    };
+};
 
 export default covid19ImpactEstimator;
+//module.exports = covid19ImpactEstimator;
