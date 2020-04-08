3a364a0c7e87ad814b4660612d49045f16bd60de
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-08 23:37:47 +0530

    =indentation done

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,15 @@
 const covid19ImpactEstimator = (data) => {
-    const given = data;
-    return {
-        data: given,
-        impact: {
-            currentlyInfected: given.reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * Math.pow(2, given.timeToElapse / 3)
-        },
-        severeImpact: {
-            currentlyInfected: given.reportedCases * 50,
-            infectionsByRequestedTime: currentlyInfected * Math.pow(2, given.timeToElapse / 3)
-        }
-    };
+  const given = data;
+  return {
+    data: given,
+    impact: {
+      currentlyInfected: given.reportedCases * 10,
+      infectionsByRequestedTime: this.currentlyInfected * (2 ** (given.timeToElapse / 3))
+    },
+    severeImpact: {
+      currentlyInfected: given.reportedCases * 50,
+      infectionsByRequestedTime: this.currentlyInfected * (2 ** (given.timeToElapse / 3))
+    }
+  };
 };
 export default covid19ImpactEstimator;
