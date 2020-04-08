32826e4319caba033b9dd51a5a59e913808f7d8c
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-09 01:36:31 +0530

    =time added1

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,13 @@
 const covid19ImpactEstimator = (data) => {
   const given = data;
-  var time = 0;
-  if(given.periodType == "days"){
+  let time = 0;
+  if (given.periodType === 'days') {
     time = given.timeToElapse / 3;
   }
-  if(given.periodType == "weeks"){
+  if (given.periodType === 'weeks') {
     time = (given.timeToElapse * 7) / 3;
   }
-  if(given.periodType == "months"){
+  if (given.periodType === 'months') {
     time = (given.timeToElapse * 30) / 3;
   }
   return {
