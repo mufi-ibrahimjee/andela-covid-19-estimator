0f3f0b15193dcb1396a0f5771b5f52d603a1cb11
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-09 13:23:40 +0530

    =time added2

--- src/estimator.js
+++ src/estimator.js
@@ -2,13 +2,13 @@ const covid19ImpactEstimator = (data) => {
   const given = data;
   let time = 0;
   if (given.periodType === 'days') {
-    time = given.timeToElapse / 3;
+    time = Math.floor(given.timeToElapse / 3);
   }
   if (given.periodType === 'weeks') {
-    time = (given.timeToElapse * 7) / 3;
+    time = Math.floor((given.timeToElapse * 7) / 3);
   }
   if (given.periodType === 'months') {
-    time = (given.timeToElapse * 30) / 3;
+    time = Math.floor((given.timeToElapse * 30) / 3);
   }
   return {
     data: given,
