782c0b375832df70c5646a55f3dee9fac4466b97
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-10 05:08:42 +0530

    =challenge5-1

---
+++ src/utils/responsetime.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+function logResponseTime(req, res, next) {
+  const startHrTime = process.hrtime();
+  res.on("finish", () => {
+    const elapsedHrTime = process.hrtime(startHrTime);
+    const elapsedTimeInMs = (elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6) * 100;
+    if(req.path !== '/api/v1/on-covid-19/logs')
+    {
+      addLog(req.method, req.path, res.statusCode, elapsedTimeInMs);
+    }
+  });
+  next();
+}
+const addLog = (method, path, statusCode, elapsedTimeInMs) => {
+  const log = method + '\t\t' + path + '\t\t' 
+  const logs = log + statusCode + '\t\t' + elapsedTimeInMs + ' ' + "ms"
+  saveLogs(logs)
+}
+const saveLogs = (logs) => {
+  const dataJSON = logs + '\r\n'
+  fs.appendFileSync('logs.json', dataJSON)
+}
+
+module.exports = logResponseTime;
