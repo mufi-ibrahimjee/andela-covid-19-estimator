7901e80a434dc1824cb57b15b0ed5e6283ac600f
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-10 16:50:34 +0530

    =challenge5-17

--- src/estimator.js
+++ src/estimator.js
@@ -40,7 +40,7 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: Math.ceil((0.35 * given.totalHospitalBeds) - (scbrts)),
       casesForICUByRequestedTime: Math.ceil(0.05 * ibrts),
       casesForVentilatorsByRequestedTime: Math.ceil(0.02 * ibrts),
-      dollarsInFlight: Math.round(difhs* 100) / 100
+      dollarsInFlight: Math.round(difhs * 100) / 100
 
     }
   };
