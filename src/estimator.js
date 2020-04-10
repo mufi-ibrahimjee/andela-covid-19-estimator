dc08ec1977e3cb400cccdd94b1328ae8d853311a
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2020-04-10 23:12:33 +0530

    =challenge5-27

--- src/estimator.js
+++ src/estimator.js
@@ -2,25 +2,25 @@ const covid19ImpactEstimator = (data) => {
   const given = data;
   let time = 0;
   if (given.periodType === 'days') {
-    time = Math.floor(given.timeToElapse / 3);
+    time = Math.trunc(given.timeToElapse / 3);
   }
   if (given.periodType === 'weeks') {
-    time = Math.floor((given.timeToElapse * 7) / 3);
+    time = Math.trunc((given.timeToElapse * 7) / 3);
   }
   if (given.periodType === 'months') {
-    time = Math.floor((given.timeToElapse * 30) / 3);
+    time = Math.trunc((given.timeToElapse * 30) / 3);
   }
   const cii = given.reportedCases * 10;
   const ibrti = cii * (2 ** time);
-  const scbrti = Math.floor(0.15 * ibrti);
-  const calci = Math.trunc(ibrti * given.region.avgDailyIncomePopulation);
+  const scbrti = 0.15 * ibrti;
+  const calci = ibrti * given.region.avgDailyIncomePopulation;
   const cis = given.reportedCases * 50;
   const ibrts = cis * (2 ** time);
-  const scbrts = Math.floor(0.15 * ibrts);
-  const calcs = Math.trunc(ibrts * given.region.avgDailyIncomePopulation);
+  const scbrts = 0.15 * ibrts;
+  const calcs = ibrts * given.region.avgDailyIncomePopulation;
   const time1 = time * 3;
-  const difhi = Math.floor((calci * given.region.avgDailyIncomeInUSD) / time1);
-  const difhs = Math.floor((calcs * given.region.avgDailyIncomeInUSD) / time1);
+  const difhi = parseFloat(((calci * given.region.avgDailyIncomeInUSD) / time1).toFixed(2));
+  const difhs = parseFloat(((calcs * given.region.avgDailyIncomeInUSD) / time1).toFixed(2));
 
   return {
     data: given,
@@ -28,19 +28,19 @@ const covid19ImpactEstimator = (data) => {
       currentlyInfected: cii,
       infectionsByRequestedTime: ibrti,
       severeCasesByRequestedTime: scbrti,
-      hospitalBedsByRequestedTime: Math.ceil((0.35 * given.totalHospitalBeds) - (scbrti)),
-      casesForICUByRequestedTime: Math.floor(0.05 * ibrti),
-      casesForVentilatorsByRequestedTime: Math.floor(0.02 * ibrti),
-      dollarsInFlight: (Math.round(difhi * 100) / 100)
+      hospitalBedsByRequestedTime: Math.trunc((0.35 * given.totalHospitalBeds) - (scbrti)),
+      casesForICUByRequestedTime: Math.trunc(0.05 * ibrti),
+      casesForVentilatorsByRequestedTime: Math.trunc(0.02 * ibrti),
+      dollarsInFlight: difhi
     },
     severeImpact: {
       currentlyInfected: cis,
       infectionsByRequestedTime: ibrts,
       severeCasesByRequestedTime: scbrts,
-      hospitalBedsByRequestedTime: Math.ceil((0.35 * given.totalHospitalBeds) - (scbrts)),
-      casesForICUByRequestedTime: Math.floor(0.05 * ibrts),
-      casesForVentilatorsByRequestedTime: Math.floor(0.02 * ibrts),
-      dollarsInFlight: (Math.round(difhs * 100) / 100)
+      hospitalBedsByRequestedTime: Math.trunc((0.35 * given.totalHospitalBeds) - (scbrts)),
+      casesForICUByRequestedTime: Math.trunc(0.05 * ibrts),
+      casesForVentilatorsByRequestedTime: Math.trunc(0.02 * ibrts),
+      dollarsInFlight: difhs
 
     }
   };
