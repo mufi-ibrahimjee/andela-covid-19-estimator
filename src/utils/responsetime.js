5c9b2794796c061f6e8d75b765bdc56fea65e817
mufi-ibrahimjee <mufaddal_jamaly2000@yahoo.com>
2020-04-13 02:08:45 +0530

    added padding

--- src/utils/responsetime.js
+++ src/utils/responsetime.js
@@ -1,24 +1,31 @@
 const fs = require('fs');
 
 const saveLogs = (logs) => {
-  const a = '\r\n';
-  const dataJSON = `${logs} ${a}`;
+  const a = '\n';
+  const dataJSON = `${logs}${a}`;
   fs.appendFileSync('logs.json', dataJSON);
 };
 const addLog = (method, path, statusCode, elapsedTimeInMs) => {
   const a = '\t\t';
-  const log = `${method} ${a} ${path} ${a}`;
-  const logs = `${log} ${statusCode} ${a} ${elapsedTimeInMs} ms`;
+  const log = `${method}${a}${path}${a}`;
+  const logs = `${log}${statusCode}${a}${elapsedTimeInMs}ms`;
   saveLogs(logs);
 };
 function logResponseTime(req, res, next) {
   const startHrTime = process.hrtime();
+  let path = '';
   res.on('finish', () => {
-    const elapsedHrTime = process.hrtime(startHrTime);
-    const elapsedTimeInMs = (elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6) * 100;
-    if (req.path !== '/api/v1/on-covid-19/logs') {
-      addLog(req.method, req.path, res.statusCode, elapsedTimeInMs);
+    const elapsedHrTim = process.hrtime(startHrTime);
+    const elapsedTimeInM = Math.round(elapsedHrTim[0] * 1000 + elapsedHrTim[1] / 1e6);
+    const elapsedHrTime = elapsedHrTim.toString();
+    const elapsedTimeInMs = elapsedTimeInM.toString().padStart(2, 0);
+    if(req.path === '/api/v1/on-covid-19/') {
+      path = '/api/v1/on-covid-19';
     }
+    else {
+      path = req.path;
+    }
+    addLog(req.method, path, res.statusCode, elapsedTimeInMs);
   });
   next();
 }
